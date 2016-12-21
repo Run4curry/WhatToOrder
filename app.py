582d12daff10a2feb7fe7722f7dba2b09bc39d71
Shiva Kannan <s1kannan@ucsd.edu>
2016-12-21 01:30:20 -0800

    experimenting with requests, and more project structure

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
-from flask import Flask , render_template 
+from flask import Flask , render_template , jsonify , request, url_for , redirect
 from bs4 import BeautifulSoup 
 from urllib2 import urlopen
 import json 
+from Menu import GetMenu
+from Review import GetReviews
+from Whatfoods import Foodlist 
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return render_template('main.html')
+	return 'Hello swag!'
 
-@app.route('/<username>')
+@app.route('/user/<username>', methods=["GET","POST"])
 def swag(username):
-	return 'Hello %s swagwefwef' % username
+
+	if request.method == "GET":
+		people = []
+	if request.method == "POST":
+		people = ['Shiva','Aaron','Justin']
+
+	return render_template('main.html',name=username,people=people)
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
